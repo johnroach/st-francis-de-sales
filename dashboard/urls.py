fe383be866c258b0d3e0484c369244cc5eaf349c
JohnRoach <johnroach1985@gmail.com>
2016-01-01 15:12:55 -0500

    Removed unnecessary slashes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'/posts$', views.posts, name='posts'),
-    url(r'/logout$', views.logout_user, name='logout_user'),
-    url(r'/login$', views.login_user, name="login_user"),
+    url(r'posts$', views.posts, name='posts'),
+    url(r'logout$', views.logout_user, name='logout_user'),
+    url(r'login$', views.login_user, name="login_user"),
 ]
