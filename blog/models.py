12837ec8639cf195ce70c5fc5b9b5f999e746469
JohnRoach <johnroach1985@gmail.com>
2016-01-02 17:43:53 -0500

    Added draft capabilities to blog

--- blog/models.py
+++ blog/models.py
@@ -3,42 +3,44 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    date = models.DateField('date published')
-    time = models.TimeField()
-    creation_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
-    title = models.TextField(max_length=255)
-    text = models.TextField()
-    slug = models.TextField(max_length=255)
-    modified_by = models.ForeignKey(User, related_name="post_modified_by")
     created_by = models.ForeignKey(User, related_name="post_created_by")
+    creation_time = models.DateTimeField()
+    date = models.DateField('date published')
+    modified_by = models.ForeignKey(User, related_name="post_modified_by")
+    modified_time = models.DateTimeField()
+    published = models.BooleanField(default=False)
+    slug = models.TextField(max_length=255)
+    text = models.TextField()
+    time = models.TimeField()
+    title = models.TextField(max_length=255)
 
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
-    date = models.DateField('date published')
+    approved = models.BooleanField(default=False)
+    comment = models.TextField(max_length=2000)
     creation_time = models.DateTimeField()
+    date = models.DateField('date published')
     email = models.EmailField()
     name = models.CharField(max_length=25)
     parent = models.ForeignKey("self", related_name="parent_comment",
         default=None, null=True, blank=True)
     post = models.ForeignKey(Post, related_name="parent_post")
-    comment = models.TextField(max_length=2000)
-    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.comment[:255]
 
 class Page(models.Model):
-    title = models.TextField(max_length=255)
+    created_by = models.ForeignKey(User, related_name="page_created_by")
+    creation_time = models.DateTimeField()
+    modified_by = models.ForeignKey(User, related_name="page_modified_by")
+    modified_time = models.DateTimeField()
+    published = models.BooleanField(default=False)
+    rank = models.IntegerField()
     slug = models.TextField(max_length=255)
     text = models.TextField()
-    rank = models.IntegerField()
-    creation_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
-    modified_by = models.ForeignKey(User, related_name="page_modified_by")
-    created_by = models.ForeignKey(User, related_name="page_created_by")
+    title = models.TextField(max_length=255)
 
     def __str__(self):
         return self.title
