f305c4cf20395924e4701b2dad6747a2ce62753a
JohnRoach <johnroach1985@gmail.com>
2016-01-01 22:28:57 -0500

    Minimized ckeditor

--- static/dashboard/js/ckeditor/build-config.js
+++ static/dashboard/js/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/e6f495825c616bcb0f3e3666a84ae216
+ * (2) http://ckeditor.com/builder/684578be12e479ba045d7125ebdefec7
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/e6f495825c616bcb0f3e3666a84ae216
+ * (3) http://ckeditor.com/builder/download/684578be12e479ba045d7125ebdefec7
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -26,7 +26,7 @@
 
 var CKBUILDER_CONFIG = {
 	skin: 'bootstrapck',
-	preset: 'full',
+	preset: 'basic',
 	ignore: [
 		'.bender',
 		'bender.js',
@@ -50,67 +50,29 @@ var CKBUILDER_CONFIG = {
 		'tests'
 	],
 	plugins : {
-		'a11yhelp' : 1,
 		'about' : 1,
 		'basicstyles' : 1,
-		'bidi' : 1,
 		'blockquote' : 1,
 		'clipboard' : 1,
 		'codesnippet' : 1,
-		'colorbutton' : 1,
-		'colordialog' : 1,
-		'contextmenu' : 1,
-		'dialogadvtab' : 1,
-		'div' : 1,
-		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
-		'filebrowser' : 1,
-		'find' : 1,
-		'flash' : 1,
 		'floatingspace' : 1,
 		'font' : 1,
 		'format' : 1,
-		'forms' : 1,
-		'horizontalrule' : 1,
-		'htmlwriter' : 1,
-		'iframe' : 1,
 		'image' : 1,
-		'indentblock' : 1,
 		'indentlist' : 1,
-		'justify' : 1,
-		'language' : 1,
 		'link' : 1,
 		'list' : 1,
-		'liststyle' : 1,
-		'magicline' : 1,
-		'maximize' : 1,
-		'newpage' : 1,
-		'pagebreak' : 1,
-		'pastefromword' : 1,
-		'pastetext' : 1,
-		'preview' : 1,
-		'print' : 1,
+		'mathjax' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
-		'save' : 1,
-		'scayt' : 1,
-		'selectall' : 1,
-		'showblocks' : 1,
-		'showborders' : 1,
 		'smiley' : 1,
-		'sourcearea' : 1,
-		'specialchar' : 1,
-		'stylescombo' : 1,
-		'tab' : 1,
-		'table' : 1,
 		'tabletools' : 1,
-		'templates' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
-		'videodetector' : 1,
-		'wsc' : 1,
-		'wysiwygarea' : 1
+		'wysiwygarea' : 1,
+		'youtube' : 1
 	},
 	languages : {
 		'af' : 1,
